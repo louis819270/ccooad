4382e71b5974db9e8aa5730268552734852e519c
louis819270 <louis819270@gmail.com>
2017-05-25 15:46:19 +0800

    123

---
+++ OOpro/OOpro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OOpro.Startup))]
+namespace OOpro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
