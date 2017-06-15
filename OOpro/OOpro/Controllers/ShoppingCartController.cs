9ea8e27264aad96e39c4078df7f62eca1bcff26a
seconddim <benken310@gmail.com>
2017-06-15 14:53:47 +0800

    購物車 抽點數 控制器

---
+++ OOpro/OOpro/Controllers/ShoppingCartController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OOpro.Models;
+
+namespace OOpro.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private OOproDB db = new OOproDB();
+
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View(db.Item.ToList());
+        }
+
+        // GET: ShoppingCart/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Item.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: ShoppingCart/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShoppingCart/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Picture,Price,Count,Description")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Item.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        // GET: ShoppingCart/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Item.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: ShoppingCart/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Picture,Price,Count,Description")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        // GET: ShoppingCart/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Item.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: ShoppingCart/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Item.Find(id);
+            db.Item.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
