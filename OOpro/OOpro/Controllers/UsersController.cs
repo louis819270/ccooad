8120c12d5978b301e9d8044769d346e2144e48a6
louis819270 <louis819270@gmail.com>
2017-06-15 18:05:03 +0800

    新增 User Controller 然後不小心把註冊跟登入做完了QQ

---
+++ OOpro/OOpro/Controllers/UsersController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OOpro.Models;
+
+namespace OOpro.Controllers
+{
+    public class UsersController : Controller
+    {
+        private OOproDB db = new OOproDB();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.User.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "ID,Account,Password,Phone,Email,Address,Picture,LV")] User user)
+        {
+            user.LV = 0;
+            if (ModelState.IsValid)
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "ID,Account,Password,Phone,Email,Address,Picture,LV")] User user)
+        {
+            try
+            {
+                String Account = db.User.AsNoTracking().FirstOrDefault(a => a.Account == user.Account).Account;
+            }
+            catch
+            {
+                return Content("ERROR");
+            }
+            String Password = db.User.AsNoTracking().FirstOrDefault(a => a.Account == user.Account).Password;
+            if (user.Password == Password)
+            {
+                user = db.User.AsNoTracking().FirstOrDefault(a => a.Account == user.Account);
+                Session["UserID"] = user.ID;
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            else
+                return Content("ERROR");
+
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Account,Password,Phone,Email,Address,Picture,LV")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.User.Find(id);
+            db.User.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
